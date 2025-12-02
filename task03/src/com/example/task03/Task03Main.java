eb1b05e4cea254640f239c0a95d97854f8931770
Liveenjuck <vipkukov5@gmail.com>
2025-12-02 19:25:03 +0500

    task03 solve

--- task03/src/com/example/task03/Task03Main.java
+++ task03/src/com/example/task03/Task03Main.java
@@ -1,7 +1,6 @@
 package com.example.task03;
 
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.nio.charset.Charset;
 
 public class Task03Main {
@@ -15,7 +14,14 @@ public class Task03Main {
     }
 
     public static String readAsString(InputStream inputStream, Charset charset) throws IOException {
-        // your implementation here
-        return "";
+        if (inputStream == null || charset == null) throw new IllegalArgumentException();
+        InputStreamReader reader = new InputStreamReader(inputStream, charset);
+        BufferedReader bufReader = new BufferedReader(reader);
+        StringBuilder res = new StringBuilder();
+        String line;
+        while ((line = bufReader.readLine()) != null) {
+            res.append(line);
+        }
+        return res.toString();
     }
 }
